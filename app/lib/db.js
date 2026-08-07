5a83001b991728fbf4bb99520845b5764ad5b281
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2026-08-07 01:40:42 +0100

    Modification du fichier de connexion

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,25 +2,23 @@ import mysql from 'mysql2/promise';
 
 let pool;
 
-if (!global._mysqlPool) {
-    global._mysqlPool = mysql.createPool({
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT) || 4000,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        ssl: {
-            minVersion: 'TLSv1.2',
-            rejectUnauthorized: true
-        },
-        waitForConnections: true,
-        connectionLimit: 5,
-        queueLimit: 0
-    });
-}
-
-pool = global._mysqlPool;
-
 export const getConnection = () => {
+    if (!pool) {
+        pool = mysql.createPool({
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT) || 4000,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME || 'test',
+            ssl: {
+                minVersion: 'TLSv1.2',
+                rejectUnauthorized: true
+            },
+            waitForConnections: true,
+            connectionLimit: 5,
+            queueLimit: 0,
+            connectTimeout: 10000
+        });
+    }
     return pool;
 };
