c4f33e1e9d3898726d4a42ff884d4d7a572ec8a7
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2026-08-19 20:45:15 +0100

    Connexion aec ticket

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -3,101 +3,108 @@
 import { useRouter } from "next/navigation";
 
 export default function PortailCaptifEmpireLab() {
-    const router = useRouter();
+  const router = useRouter();
 
-    // Redirection directe vers la page de paiement en passant l'ID du forfait
-    const selectionnerForfait = (idForfait: number) => {
-        router.push(`/client/achat?forfait=${idForfait}`);
-    };
+  // Redirection directe vers la page de paiement en passant l'ID du forfait
+  const selectionnerForfait = (idForfait: number) => {
+    router.push(`/client/achat?forfait=${idForfait}`);
+  };
 
-    const verifForfait = () => {
-        router.push("/client/ticket");
-    };
+  const verifForfait = () => {
+    router.push("/client/ticket");
+  };
 
-    return (
-        <div className="min-h-screen max-w-[500px] mx-auto flex flex-col items-center gap-2 justify-center px-4">
-            <div className="bg-[#0070f3] px-1.5 py-1 w-full rounded-[0.5rem]">
-                <h1 className="text-white text-[28px] text-center font-bold">
-                    Empire-Lab Hotspot
-                </h1>
-            </div>
+  const achatTicket = () => {
+    router.push("/client/connexion");
+  };
 
-            {/* Message de guidage */}
-            <div className="font-bold mt-2 mb-2">
-                <h2 className="text-[18px]">Choisissez votre forfait</h2>
-            </div>
+  return (
+    <div className="min-h-screen max-w-[500px] mx-auto flex flex-col items-center gap-2 justify-center px-4">
+      <div className="bg-[#0070f3] px-1.5 py-1 w-full rounded-[0.5rem]">
+        <h1 className="text-white text-[28px] text-center font-bold">
+          Empire-Lab Hotspot
+        </h1>
+      </div>
 
-            {/* LA GRILLE DES FORFAITS */}
-            <div className="flex flex-col gap-8 w-full">
-                {/* Forfait 1 : 3 heures */}
-                <div className="flex justify-between items-center gap-2 py-[1.25rem] px-[10px] shadow-[0_2px_4px_rgba(0,0,0,0.11)] rounded-[14px] border-[1px] border-[#e5e7eb]">
-                    <div>
-                        <h3 className="font-bold text-[16px]">Forfait 3 Heures</h3>
-                        <p className="text-[12px] text-[#6b7280]">
-                            Idéal pour une vérification rapide
-                        </p>
-                        <span className="text-[22px] font-extrabold block">1 000 FC</span>
-                    </div>
-                    <button
-                        onClick={() => selectionnerForfait(1)}
-                        className="text-[18px] font-bold bg-[#0070f3] px-[10px] py-[12px] cursor-pointer text-white rounded-[10px]"
-                    >
-                        Choisir
-                    </button>
-                </div>
+      {/* Message de guidage */}
+      <div className="font-bold mt-2 mb-2">
+        <h2 className="text-[18px]">Choisissez votre forfait</h2>
+      </div>
 
-                {/* Forfait 2 : 8 Heures (Mis en valeur) */}
-                <div className="flex justify-between items-center relative gap-2 rounded-[14px] bg-white shadow-[0_4px_12px_rgba(0,112,243,0.1)] border-[2px] border-[#0070f3] py-[1.25rem] px-[10px]">
-                    <span className="absolute text-[13px]  top-[-15px] right-[15px] bg-[#0070f3] text-white py-[5px] px-[10px] rounded-[20px] tracking-wide-[5px] uppercase">
-                        Le plus vendu
-                    </span>
-                    <div className="inline gap-16">
-                        <h3 className="text-[16px] font-bold mt-0 mr-0 mb-[4px] ml-0">
-                            Forfait 24 Heures
-                        </h3>
-                        <p className="text-[12px] text-[#6b7280]">
-                            Le meilleur compromis vitesse/prix
-                        </p>
-                        <span className="text-[22px] font-extrabold block">2 500 FC</span>
-                    </div>
-                    <button
-                        onClick={() => selectionnerForfait(2)}
-                        className="text-[18px] font-bold bg-[#0070f3] px-[10px] py-[12px] cursor-pointer text-white rounded-[10px]"
-                    >
-                        Choisir
-                    </button>
-                </div>
-
-                {/* Forfait 3 : 24 Heures */}
-                <div className="flex justify-between items-center relative gap-2 rounded-[14px] bg-white shadow-[0_4px_12px_rgba(0,112,243,0.1)] shadow-[0_2px_4px_rgba(0,0,0,0.2)] border-[1px] border-[#e5e7eb] py-[1.25rem] px-[10px]">
-                    <div className="inline gap-8">
-                        <h3 className="text-[16px] font-bold mt-0 mr-0 mb-[4px] ml-0">
-                            Forfait 3 jours
-                        </h3>
-                        <p className="text-[12px] text-[#6b7280]">
-                            Tranquillité totale pendant 3 jours
-                        </p>
-                        <span className="text-[22px] font-extrabold block">5 000 FC</span>
-                    </div>
-                    <button
-                        onClick={() => selectionnerForfait(3)}
-                        className="text-[18px] font-bold bg-[#0070f3] px-[10px] cursor-pointer py-[12px] text-white rounded-[10px]"
-                    >
-                        Choisir
-                    </button>
-                </div>
-            </div>
-            <div className="grid grid-cols-2 gap-2 w-full">
-                <button className="bg-[#F8FAFC] w-full py-[0.3rem] border border-2 border-[#2563EB] text-[#2563EB] px-0 rounded-[0.35rem] mt-[10px]">
-                    Avec ticket
-                </button>
-                <button
-                    onClick={() => verifForfait()}
-                    className="bg-[#64748B] w-full py-[0.5rem] px-0 text-white rounded-[0.35rem] mt-[10px]"
-                >
-                    Vérifier mon ticket
-                </button>
-            </div>
+      {/* LA GRILLE DES FORFAITS */}
+      <div className="flex flex-col gap-8 w-full">
+        {/* Forfait 1 : 3 heures */}
+        <div className="flex justify-between items-center gap-2 py-[1.25rem] px-[10px] shadow-[0_2px_4px_rgba(0,0,0,0.11)] rounded-[14px] border-[1px] border-[#e5e7eb]">
+          <div>
+            <h3 className="font-bold text-[16px]">Forfait 3 Heures</h3>
+            <p className="text-[12px] text-[#6b7280]">
+              Idéal pour une vérification rapide
+            </p>
+            <span className="text-[22px] font-extrabold block">1 000 FC</span>
+          </div>
+          <button
+            onClick={() => selectionnerForfait(1)}
+            className="text-[18px] font-bold bg-[#0070f3] px-[10px] py-[12px] cursor-pointer text-white rounded-[10px]"
+          >
+            Choisir
+          </button>
         </div>
-    );
+
+        {/* Forfait 2 : 8 Heures (Mis en valeur) */}
+        <div className="flex justify-between items-center relative gap-2 rounded-[14px] bg-white shadow-[0_4px_12px_rgba(0,112,243,0.1)] border-[2px] border-[#0070f3] py-[1.25rem] px-[10px]">
+          <span className="absolute text-[13px]  top-[-15px] right-[15px] bg-[#0070f3] text-white py-[5px] px-[10px] rounded-[20px] tracking-wide-[5px] uppercase">
+            Le plus vendu
+          </span>
+          <div className="inline gap-16">
+            <h3 className="text-[16px] font-bold mt-0 mr-0 mb-[4px] ml-0">
+              Forfait 24 Heures
+            </h3>
+            <p className="text-[12px] text-[#6b7280]">
+              Le meilleur compromis vitesse/prix
+            </p>
+            <span className="text-[22px] font-extrabold block">2 500 FC</span>
+          </div>
+          <button
+            onClick={() => selectionnerForfait(2)}
+            className="text-[18px] font-bold bg-[#0070f3] px-[10px] py-[12px] cursor-pointer text-white rounded-[10px]"
+          >
+            Choisir
+          </button>
+        </div>
+
+        {/* Forfait 3 : 24 Heures */}
+        <div className="flex justify-between items-center relative gap-2 rounded-[14px] bg-white shadow-[0_4px_12px_rgba(0,112,243,0.1)] shadow-[0_2px_4px_rgba(0,0,0,0.2)] border-[1px] border-[#e5e7eb] py-[1.25rem] px-[10px]">
+          <div className="inline gap-8">
+            <h3 className="text-[16px] font-bold mt-0 mr-0 mb-[4px] ml-0">
+              Forfait 3 jours
+            </h3>
+            <p className="text-[12px] text-[#6b7280]">
+              Tranquillité totale pendant 3 jours
+            </p>
+            <span className="text-[22px] font-extrabold block">5 000 FC</span>
+          </div>
+          <button
+            onClick={() => selectionnerForfait(3)}
+            className="text-[18px] font-bold bg-[#0070f3] px-[10px] cursor-pointer py-[12px] text-white rounded-[10px]"
+          >
+            Choisir
+          </button>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-2 w-full">
+        <button
+          onClick={() => achatTicket()}
+          className="bg-[#F8FAFC] w-full py-[0.3rem] border border-2 border-[#2563EB] text-[#2563EB] px-0 rounded-[0.35rem] mt-[10px]"
+        >
+          Avec ticket
+        </button>
+        <button
+          onClick={() => verifForfait()}
+          className="bg-[#64748B] w-full py-[0.5rem] px-0 text-white rounded-[0.35rem] mt-[10px]"
+        >
+          Vérifier mon ticket
+        </button>
+      </div>
+    </div>
+  );
 }
